18830c24225ff4939cd5e2cef09d4e7a0531e145
yashvi-dobariya <yashvidobariya2003@gmail.com>
2025-02-22 16:08:36 +0530

    auth_1 initial commit

---
+++ src/main/java/com/javadev/auth_1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.javadev.auth_1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Auth1Application.class);
+	}
+
+}
---
+++ src/test/java/com/javadev/auth_1/Auth1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadev.auth_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
